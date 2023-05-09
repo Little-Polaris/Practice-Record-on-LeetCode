84f78a5bcf749abe95b9958ef9d72a9f6449e9da
Qi Jiahao <2375425475@qq.com>
2023-05-09 19:44:40 +0800

    29题还没做完
    这道题有什么意思呢？

--- 29两数相除/29两数相除/Main.cpp
+++ 29两数相除/29两数相除/Main.cpp
@@ -5,15 +5,40 @@ using namespace std;
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        int max = pow(2, 31) - 1;
+        if (dividend == -max - 1 && divisor == 1)
+        {
+            return -max - 1;
+        }
+        if (dividend == max && divisor == 1)
+        {
+            return max;
+        }
+        else if (dividend == max && divisor == -1)
+        {
+            return -max;
+        }
         int count = 0;
         int flag = 1;
+        int former_over_flow = 0;
+        int latter_over_flow = 0;
         if (dividend < 0)
         {
+            if (dividend == -max - 1)
+            {
+                former_over_flow = 1;
+                dividend = dividend + 1;
+            }                      
             dividend = -dividend;
-            flag = -flag;
+            flag = -flag;           
         }
         if (divisor < 0)
         {
+            if (divisor == -max - 1)
+            {
+                latter_over_flow = 1;
+                divisor = divisor + 1;
+            }
             divisor = -divisor;
             flag = -flag;
         }
@@ -32,6 +57,15 @@ public:
                 }
             }
             count++;
+            if (count == 1)
+            {
+                dividend += former_over_flow;
+                divisor += latter_over_flow;
+            }
+            if (count == max)
+            {
+                return count;
+            }
         }
         return count;
     }
@@ -39,6 +73,6 @@ public:
 
 int main()
 {
-    cout << Solution().divide(-pow(2, 31), -1) << endl;
+    cout << Solution().divide(pow(2,31)-1,2) << endl;
 	return 0;
 }
