1dd21862ec7a30b21d32aef9be7d85444e529cfb
Qi Jiahao <2375425475@qq.com>
2023-05-12 13:21:57 +0800

    29屎山终于弄完了

--- 29两数相除/29两数相除/Main.cpp
+++ 29两数相除/29两数相除/Main.cpp
@@ -13,28 +13,82 @@ public:
         {
             return 1;
         }
+        if (divisor == -pow(2, 31))
+        {
+            return 0;
+        }
+        else if (dividend == -divisor)
+        {
+            return -1;
+        }
+        if (divisor == 1)
+        {
+            return dividend;
+        }
+        else if (divisor == -1)
+        {
+            if (dividend == -pow(2, 31))
+            {
+                return pow(2, 31) - 1;
+            }
+            else
+            {
+                return -dividend;
+            }
+        }
         int flag = 1;
         if (dividend == -pow(2, 31))
         {
-            ;
+            if (divisor == -1)
+            {
+                return pow(2, 31) - 1;
+            }
+            else if (divisor == 1)
+            {
+                return dividend;
+            }
+            else if (divisor == -2)
+            {
+                return pow(2, 30);
+            }
+            else if (divisor == 2)
+            {
+                return -pow(2, 30);
+            }
+            else
+            {
+                for (int i = 1; i < 31; i++)
+                {
+                    if (divisor == pow(2, i))
+                    {
+                        return -pow(2, 31 - i);
+                    }
+                }
+            }
+            dividend = pow(2, 31) - 1;
+            flag = -flag;   
         }
         else if (dividend < 0)
         {
             dividend = -dividend;
             flag = -flag;
-        }
-        if (divisor == -pow(2, 31))
-        {
-            ;
-        }
-        else if (divisor < 0)
+        } 
+        if (divisor < 0)
         {
             divisor = -divisor;
-            flag = flag;
+            flag = -flag;
         }
         int small = divisor;
-        int big = divisor + divisor;
         int max = pow(2, 31) - 1;
+        int big;
+        if (max - divisor < divisor)
+        {
+            big = max;
+        }
+        else
+        {
+            big = divisor + divisor;
+        }
         int flag1 = 0;
         int count = 0;
         while (dividend >= big && max - big > big)        
@@ -44,7 +98,7 @@ public:
             big += big;
             count++;
         }
-        if (flag1)
+        if (flag1 || dividend >= divisor)
         {
             count = pow(2, count);
         }
@@ -66,6 +120,6 @@ public:
 
 int main()
 {
-    cout << Solution().divide(pow(2,31) - 1, 2) << endl;
+    cout << Solution().divide(-pow(2,31), 8) << endl;
 	return 0;
 }
